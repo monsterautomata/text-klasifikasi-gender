a3054f4e9965d7ab08c6156c049999e14f40e94a
fikrirazor <fikrirazor@outlook.co.id>
2019-05-10 01:54:19 +0700

    add mnnb

--- text-klasifikasi-gender.py
+++ text-klasifikasi-gender.py
@@ -6,4 +6,18 @@ Created on Fri May 10 01:28:51 2019
 """
 
 import pandas as pd 
-df = pd.read_csv("komentar kaskus gender.csv")
+from sklearn.naive_bayes import MultinomialNB
+df = pd.read_csv("komentar kaskus gender.csv",encoding='latin1')
+
+x = df.iloc[:,0]
+y = df.iloc[:,1]
+
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(x)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+clf = MultinomialNB()
+clf.fit(X_train,y_train)
+predict=clf.predict(X_test)
+clf.score(X_train,y_train)
